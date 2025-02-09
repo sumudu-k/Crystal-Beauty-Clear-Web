a38caf5a5630c7ee001754928128a9fe00a6a7d4
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-09 19:35:24 +0530

    Remove product-related functionality and routes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
@@ -40,7 +39,6 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 
 app.listen(5000, () => {
