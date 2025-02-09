142cc0796cd913a21331f9ee7d956ca99b84aba1
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-09 17:41:45 +0530

    Refactor database connection to use environment variable for MongoDB URI😍

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import mongoose from 'mongoose';
 import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 
-const mongoUrl = "mongodb+srv://admin:123@cluster0.xgkqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoUrl = process.env.MONGO_DB_URI;
 
 mongoose.connect(mongoUrl, {});
 const connection = mongoose.connection;
