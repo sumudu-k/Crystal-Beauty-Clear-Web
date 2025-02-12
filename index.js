4471ac017ba8c9376166d378835abd4b8ad9780b
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-12 15:56:49 +0530

    import product router and add product router endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
+import productRouter from './routes/productRouter.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -40,6 +41,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
