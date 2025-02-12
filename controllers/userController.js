60b6c07000c0d52809bbfe9790c8bded7443fdc9
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-12 15:12:11 +0530

    Creates a function to check  the user is customer or  an admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,3 +75,24 @@ export function deleteUser() {
     })
 };
 
+
+
+export function isAdmin(req) {
+    if (req.user == null) {
+        return false;
+    }
+    if (req.user.type != "admin") {
+        return false;
+    }
+    return true;
+}
+
+export function isCustomer(req) {
+    if (req.user == null) {
+        return false;
+    }
+    if (req.user.type != "customer") {
+        return false;
+    }
+    return true;
+}
