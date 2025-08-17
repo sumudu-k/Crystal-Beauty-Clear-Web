18814f14d899dda7d358f6eef5739378891a5c47
sumudu-k <lakshankulathunga21@gmail.com>
2025-08-17 08:41:35 +0530

    created the function to get product details by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -49,5 +49,13 @@ export function updateProduct(req,res){
     })
 }
 
+export async function getProductById(req, res){
+    try{
+         const productId = req.params.productId;
+    const product = await Product.findOne({productId:productId})
+    res.json(product);
+    }catch(e){
+        res.status(500).json({e})
+    }
 
-
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProducts, createProduct, deleteProduct,updateProduct } from '../controllers/productController.js';
+import { getProducts, createProduct, deleteProduct,updateProduct,getProductById } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/', createProduct);
 productRouter.get('/', getProducts)
+productRouter.get('/:productId', getProductById);
 productRouter.delete('/:productId', deleteProduct);
 productRouter.put('/:productId', updateProduct);
 
